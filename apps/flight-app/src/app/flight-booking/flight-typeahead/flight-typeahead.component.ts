5943892c8d1cbbad5cae3d6907917daa65e577e2
michael.zikes <michael.zikes@intauria.com>
2021-06-01 14:29:31 +0200

    RxJS Typeahead

---
+++ apps/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
@@ -0,0 +1,73 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Flight } from '@flight-workspace/flight-lib';
+import { Observable, of } from 'rxjs';
+import { debounceTime, delay, distinctUntilChanged, filter, map, switchMap, tap, withLatestFrom } from 'rxjs/operators';
+
+@Component({
+  selector: 'flight-workspace-flight-typeahead',
+  templateUrl: './flight-typeahead.component.html',
+  styleUrls: ['./flight-typeahead.component.css']
+})
+export class FlightTypeaheadComponent implements OnInit {
+  control = new FormControl();
+  // Result Stream -> Template subscription
+  flights$: Observable<Flight[]>;
+  loading: boolean;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    const dataProvider = of({
+      from: 'New York',
+      to: 'Paris'
+    });
+
+    // Stream 1: Control Filter Value
+    //   - Trigger
+    //   - Data Provider
+    this.flights$ = this.control.valueChanges.pipe(
+      withLatestFrom(dataProvider),
+      // Filter START
+      map(([trigger, state]) => trigger),
+      filter(from => from.length > 2),
+      debounceTime(300),
+      distinctUntilChanged(),
+      // Filter END
+      // Side Effect
+      tap(_ => this.loading = true),
+      // Connect 2nd Stream
+      switchMap(from => this.load(from)),
+      delay(1000),
+      // Side Effect
+      tap(_ => this.loading = false)
+    );
+
+    const flightsCount$ = this.flights$.pipe(
+      map(flights => flights.length)
+    );
+
+    flightsCount$.subscribe(console.log);
+  }
+
+  // Stream 2: Server Call (Data access)
+  load(from: string): Observable<Flight[]>  {
+    const url = "http://www.angular.at/api/flight";
+
+    const params = new HttpParams()
+                        .set('from', from);
+
+    const headers = new HttpHeaders()
+                        .set('Accept', 'application/json');
+
+    // return this.http.get<Flight[]>(url, {params, headers});
+    return of([{
+      id: 999,
+      from: 'New York',
+      to: 'Paris',
+      date: new Date().toISOString(),
+      delayed: true
+    }]);
+  }
+}
